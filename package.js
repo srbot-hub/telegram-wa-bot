f887709d9f32eccef906e898aad860f1c7e1abb9
srbot-hub <srgithub12@gmail.com>
2025-10-07 12:26:40 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "telegram-bot",
+  "version": "1.0.0",
+  "description": "A Telegram bot that can send messages to WhatsApp numbers and auto-reply questions.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.4.5",
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
